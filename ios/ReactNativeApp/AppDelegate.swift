7215f77f1161818b949d48d9ebbcf72c0bcd9d9c
tsaromkar <omkarmahadik357@gmail.com>
2025-09-20 19:29:19 +0530

    setup ios

--- ios/ReactNativeApp/AppDelegate.swift
+++ ios/ReactNativeApp/AppDelegate.swift
@@ -2,6 +2,7 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -28,7 +29,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       in: window,
       launchOptions: launchOptions
     )
-
+    
+    FirebaseApp.configure()
+    
     return true
   }
 }
