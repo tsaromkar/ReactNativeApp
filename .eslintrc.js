2ab0bb9dfde1460bbdef13be82db7d96eab018e8
tsaromkar <omkarmahadik357@gmail.com>
2025-11-21 13:17:49 +0530

    add aliases to eslintrc and jsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,19 @@ module.exports = {
   root: true,
   extends: '@react-native',
   plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      'babel-module': {
+        root: ['./src'],
+        alias: {
+          '@components': './src/Components',
+          '@utils': './src/Utils',
+          '@network': './src/Network',
+          '@contexts': './src/Contexts',
+        },
+      },
+    },
+  },
   rules: {
     'import/no-unresolved': 'error',
   },
